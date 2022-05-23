b61c24b0bf43368beb16699e1aae25f279249eb6
Mamaev [FancaR] Alexandr <fancarster@gmail.com>
2022-05-23 15:13:12 +0300

    OpenOuiDb panic removed, return err

--- main.go
+++ main.go
@@ -1,77 +1,72 @@
 package tzspanalyser
 
 import (
-	"errors"
-    "github.com/rs/tzsp"
+    "encoding/hex"
+    "errors"
     "github.com/google/gopacket"
     "github.com/google/gopacket/layers"
     "github.com/klauspost/oui"
-    "encoding/hex"
-//    "net"
-//	"fmt"
+    "github.com/rs/tzsp"
+    //    "net"
+    //  "fmt"
 )
 
 var (
-	OUI_db oui.StaticDB
+    OUI_db oui.StaticDB
 )
 
-func OpenOuiDb(filename string) {
-	db, err := oui.OpenStaticFile(filename)
+func OpenOuiDb(filename string) error {
+    db, err := oui.OpenStaticFile(filename)
+    if err != nil {
+        return err
+    }
 
-	if err != nil {
-		panic(err)
-	} else {
-		OUI_db = db
-	}
+    OUI_db = db
+    return nil
 }
 
+func VendorByMac(mac []byte) (string, error) {
 
-func VendorByMac(mac []byte) (string,error) {
+    hw := oui.HardwareAddr{mac[0], mac[1], mac[2]}
+    entry, err := OUI_db.LookUp(hw)
 
-	hw := oui.HardwareAddr{mac[0], mac[1], mac[2]}
-	entry, err := OUI_db.LookUp(hw)
+    if err == oui.ErrNotFound {
+        return "unknown", nil
 
-	if err == oui.ErrNotFound {
-		return "unknown",nil
-
-	} else if err != nil {
-		return "no data",err
-	} else {
-		return entry.Manufacturer,nil
-	}	
+    } else if err != nil {
+        return "no data", err
+    } else {
+        return entry.Manufacturer, nil
+    }
 }
 
-
-
-func Parse(packet []uint8) (map[string]interface{},error) {
-	result := map[string]interface{}{}
+func Parse(packet []uint8) (map[string]interface{}, error) {
+    result := map[string]interface{}{}
     p, err := tzsp.Parse(packet)
     if err != nil {
-    	return result, err
+        return result, err
     }
 
-    if p.Header.Proto.String() != "ProtoIEEE80211"{
-    	return result, errors.New("Unknown protocol incapsulated: "+p.Header.Proto.String())
+    if p.Header.Proto.String() != "ProtoIEEE80211" {
+        return result, errors.New("Unknown protocol incapsulated: " + p.Header.Proto.String())
     }
 
-	result = RadioTap(p)
+    result = RadioTap(p)
 
     if result["sensor_id"] == "" || result["sensor_id"] == nil {
-        return result,errors.New("No sensor-id in RadioTap. Might be some old firmware?")
+        return result, errors.New("No sensor-id in RadioTap. Might be some old firmware?")
     }
 
-	return result,nil
+    return result, nil
 
 }
 
-
-
 func RadioTap(p tzsp.Packet) map[string]interface{} {
-	// meta header. Look through radiotap interface data that contains in tzsp header
+    // meta header. Look through radiotap interface data that contains in tzsp header
     //fmt.Println("Dot11 flame have been detecded")
-    result := map[string]interface{}{"dot11header":p.Data}
+    result := map[string]interface{}{"dot11header": p.Data}
     for _, t := range p.Tags {
-    	//fmt.Println("radio_tap:",t.Type.String(),t.Data)
+        //fmt.Println("radio_tap:",t.Type.String(),t.Data)
         switch t.Type.String() {
         case "TagWLANRadioHDRSerial":
             result["sensor_id"] = hex.EncodeToString(t.Data)
@@ -87,33 +82,31 @@ func RadioTap(p tzsp.Packet) map[string]interface{} {
 }
 
 func ParseDot11(data []byte) layers.Dot11 {
-	// wi-fi header is parsing by gopacket layers
+    // wi-fi header is parsing by gopacket layers
 
     var Dot11 layers.Dot11 //Dot11
-        //var CtrlBlockAck layers.Dot11CtrlBlockAck
-        //var Ctrl layers.Dot11Ctrl     
+    //var CtrlBlockAck layers.Dot11CtrlBlockAck
+    //var Ctrl layers.Dot11Ctrl
     Decodedlayers := []gopacket.LayerType{}
     parser := gopacket.NewDecodingLayerParser(layers.LayerTypeDot11, &Dot11) //, &CtrlBlockAck, &Ctrl
     //fmt.Println(parser)
     parser.DecodeLayers(data, &Decodedlayers) // packet.Data()
 
-        // if err != nil {
-        //     fmt.Println("Trouble decoding layers: ", err)
-        // }
+    // if err != nil {
+    //     fmt.Println("Trouble decoding layers: ", err)
+    // }
 
     return Dot11
 }
 
-
 func BitDisabled(data byte, position uint) bool {
-	return data & (1 << position) == 0
+    return data&(1<<position) == 0
 }
 
-
 func MacIsUnicast(mac []byte) bool {
-	return BitDisabled(mac[0],0)
+    return BitDisabled(mac[0], 0)
 }
 
 func MacAUIisUnique(mac []byte) bool {
-	return BitDisabled(mac[0],1)
+    return BitDisabled(mac[0], 1)
 }
